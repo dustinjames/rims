d5597d0684b112c793828f239e296a5831f816de
Dustin James <dustin.james@hotmail.com>
2014-07-15 15:41:50 -0500

    Added Devise, Header Partial and Home Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
 end
+
+def home
+
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Rims::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Required for Heroku
+  #Set this to your actual host when deploying into production
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
