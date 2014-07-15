b2f59394fc6cf1ffa4c835f69611de3e11d3d4b3
Dustin James <dustin.james@hotmail.com>
2014-07-15 14:29:16 -0500

    Initial Rims Commit with a modified gitignore file to exclude DS_store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rims::Application.config.secret_key_base = '510573af376cec39490b345ef9ed4f52fae63a2aa3ee46fc44fe1102c096bbc519637546b89c15a8d1d21a0c3860616cb7844c407d18cbaa863a6d3139aa3110'
